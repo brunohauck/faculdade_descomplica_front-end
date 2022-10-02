520ef6d57bd71d263f98a86377f4176b7205b1dc
Bruno Hauck <brunohauck@Brunos-MacBook-Air.local>
2022-10-02 05:57:14 -0300

    Subcomponentes e componente header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +9,7 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  @Input() nome:string = '';
   ngOnInit(): void {
   }
 
